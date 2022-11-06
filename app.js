4afda4bfa9eedd3d147fe76fbe93627c2fde6967
Mohd-Haseeb <mohd.haseeb440@gmail.com>
2022-11-06 13:02:40 +0530

    worked on /signin endpoint

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/signup', async (req, res, next) => {
         const token = jwt.sign({
             id : createdUser._id,
             email
-        }, 'mysecretstring', {expiresIn : '2h'})
+        }, process.env.SECRET_KEY, {expiresIn : '2h'})
 
 
 
@@ -82,6 +82,67 @@ app.post('/signup', async (req, res, next) => {
     
 })
 
+
+app.post('/signin', async (req, res, next) => {
+
+    try {
+        
+        // collecrt information from frontend
+        const { email, password } = req.body
+
+        if (!(email && password)){
+            res.status(400).send("All details must be entered!!")
+        }
+
+        // get user details
+        const user = await User.findOne({email})
+
+        if (!user){
+            res.send(400).send("User doesn't exist. Please register first!!!")
+        }
+
+        // NOTE ONE WAY OF COMPARING PASSWORD
+        /*
+        const encryptedPassword = await bcrypt.hash(password, 10) 
+        
+        if (!(user.password === encryptedPassword)){
+
+        }
+        */
+
+        // ANOTHER WAY
+        if (user && (await bcrypt.compare(password, user.password))){
+            const token = jwt.sign({
+                id : user._id,
+                email
+            }, process.env.SECRET_KEY, {expiresIn:'2h'})
+
+            user.password = undefined
+
+            user.token = token
+
+            const options = {
+                expires : new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly : true
+            }
+
+            res.status(200).cookie('token', token, options).json({
+                success : true,
+                token,
+                user
+            })
+
+        }
+
+        res.status(400).send("Password is incorrect!!!!")
+
+    } catch (error) {
+        
+        console.log(error)
+
+    }
+
+})
 
 
 
